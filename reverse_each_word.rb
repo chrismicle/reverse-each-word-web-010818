2da3f71b38373728cbc7510f456e9e9a5ed752b6
Christina Collura <cmcollura@gmail.com>
2018-01-03 17:03:59 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,10 @@ def reverse_each_word(sentence)
   new_array.join(" ")
 end
 
+def reverse_each_word(sentence)
+  new_array = sentence.split
+  new_array.collect do |word|
+    word.reverse!
+  end
+  new_array.join(" ")
+end
